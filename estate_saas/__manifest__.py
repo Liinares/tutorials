f37d50b4912b3a39c80d3fda95d9cc732054398d
Miguel Angel Linares <miguellinares908@gmail.com>
2025-03-27 15:48:45 +0100

    write importable modules

---
+++ estate_saas/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'estate_saas',
+    'depends': ['base', 'base_import_module'],
+    'data': [
+        'models/estate_saas_property_offer.xml',
+        'models/estate_saas_property.xml',
+        'models/estate_saas_property_type.xml',
+        'models/estate_saas_property_tag.xml',
+        'security/ir.model.access.csv',
+        'views/estate_saas_property_views.xml',
+        'views/estate_saas_property_type_views.xml',
+        'views/estate_saas_property_tag_views.xml',
+        'views/estate_saas_property_offer_views.xml',
+        'views/estate_saas_menus.xml'
+    ]
+}
