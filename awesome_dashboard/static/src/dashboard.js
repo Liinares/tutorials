98d95e8f3871e8b62e029b3635b1243835aac0fc
Miguel Angel Linares <miguellinares908@gmail.com>
2025-07-08 09:33:15 +0200

    dashboard changes

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,10 +1,45 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, onWillStart, useState } from "@odoo/owl";
+import { Layout } from "@web/search/layout";
 import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
+import { DashboardItem } from "./dashboardItem";
+import { rpc } from "@web/core/network/rpc";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+
+    static components = {
+        Layout,
+        DashboardItem,
+    };
+
+    setup() {
+        this.action = useService("action");
+
+        onWillStart(async () => {
+            const result = await rpc("/awesome_dashboard/statistics");
+
+            this.items = result
+        });
+    }
+
+    openCustomerView() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    openLeads() {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "All leads",
+            res_model: "crm.lead",
+            views: [
+                [false, "list"],
+                [false, "form"],
+            ],
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
